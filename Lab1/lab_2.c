0dfbebe23be35a7cd312388b1a86144cef3fb1ff
Abhay Varshney <varshneyabhay@gmail.com>
2018-10-03 15:41:57 -0700

    Update lab_2.c

--- Lab1/lab_2.c
+++ Lab1/lab_2.c
@@ -8,7 +8,7 @@ int main() {
     for (i = 0; i < processes; i++) {
         if (fork() != 0) {
             // wait for process termination/change state
-            e(-1, &status, 0); /* parent */
+            waitpid(-1, &status, 0); /* parent */
             exit(0);
         } else printf("Parent process id - [%d] Child process id - [%d]\n" , getppid(), getpid());
     };
